82eb1c8315478556e96ea9fa3d0c41414205b0e2
Vidhya <vidhyamrobert.29@gmail.com>
2025-08-14 00:02:16 +0530

    js core 2

--- coreJs/image slider/imgSLide.js
+++ coreJs/image slider/imgSLide.js
@@ -1,10 +1,18 @@
-const imges = ["img1.jpg", "img2.jpg", img3.jpg];
+const images = ["img1.jpg", "img2.jpg", img3.jpg];
 let index = 0;
 
 const sliderImg = document.querySelector("#sliderImg");
 const prevBtn = document.querySelector("#prev");
 const nextBtn = document.querySelector("#next");
 // function to change image
-const chamgeImg = () =>{
-  
-}
+const changeImage = () =>{
+  sliderImg.setAttribute("src", images[index]);
+};
+nextBtn.addEventListener("click", ()=>{
+  index = (index+1)% images.length;
+  changeImage();
+});
+prevBtn.addEventListener("click", ()=>{
+  index = (index-1 + images.length) % images.length;
+  changeImage();
+});
