4945e29baf103b439a48ce04031e94b6e8f86954
Vidhya <vidhyamrobert.29@gmail.com>
2025-08-20 21:46:21 +0530

    js wrap

---
+++ prereq React/Click/clicking.js
@@ -0,0 +1,251 @@
+// function changeColor(){
+//   const colors = ["Green", "Yellow", "blue", "orange"];
+//   document.body.style.backgroundColor = 
+//   colors[Math.floor(Math.random()* colors.length)];
+//   // colors.length is here 4 
+//   // so multiplying 4 gives a random number between 0 and 4
+//   // colors[Math.floor(Math.random()* colors.length)] picks a random index from the "colors" array
+// }
+
+// function toggleText(){
+//   const paragraph = document.getElementById("text");
+//   paragraph.style.display = paragraph.style.display === "none" ? "block" : "none";
+// }
+// function add(){
+//   const n1Input = document.getElementById("num1");
+//   const n2Input = document.getElementById("num2");
+
+//   const n1 =parseFloat(n1Input.value);
+//   const n2 = parseFloat(n2Input.value);
+//   // parseFloat() is a JS function that converts a string into a decimal number. Input fields always return string values when we read them with .value
+//   document.getElementById("result").textContent = "Result:" +(n1+n2);
+
+//   n1Input.value = "";
+//   n2Input.value = "";
+// }
+// let count = 0;
+// function increase(){
+//   count++;
+//   document.getElementById("count").textContent = count;
+// }
+// function decrease(){
+//   count--;
+//   document.getElementById("count").textContent = count;
+// }
+// function reset(){
+//   count = 0;
+//   document.getElementById("count").textContent = count;
+// }
+// function turnOn(){
+//   document.getElementById("bulb").src = "https://www.w3schools.com/js/pic_bulbon.gif";
+// }
+// function turnOff(){
+//   document.getElementById("bulb").src="https://www.w3schools.com/js/pic_bulboff.gif";
+// }
+// function calc(op){
+// const number1 = parseFloat(document.getElementById("n1").value);
+// const number2 = parseFloat(document.getElementById("n2").value);
+// const ress = document.getElementById("res");
+// let result;
+// if(op === 'add') result=number1+number2;
+// if(op === 'sub') result= number1-number2;
+// if(op === 'mul') result=number1*number2;
+// if(op === 'div') result=number1/number2;
+// ress.textContent = "Result: "+result
+// }
+// const secret = Math.floor(Math.random() * 20) +1;
+// const result = document.getElementById("msg");
+// function Check(){
+//   const g = parsesFloat(document.getElementById("guess").value);
+//   result.textContent = (g === secret) ? "🎉correct" : "❌Try Agin"
+// }
+// function ans(choice){
+//   document.getElementById("result").textContent =
+//   (choice === 'javascript') ? "Correct" : "wrong";
+// }
+
+// const quotes = [
+//   "Do not pray for easy lives, pray to be stronger men",
+//   "Code is like humor. when you have to explain it, it is bad.",
+//   "First, solve the problem. Then write the code",
+//   "Experience is the name everyone gives to their mistakes"
+// ];
+
+
+// function newQuote(){
+//   document.getElementById("quote").textContent =
+//   quotes[Math.floor(Math.random()*quotes.length)];
+// }
+
+// let timer;
+// function start(){
+//   timer=setInterval(()=>{
+//     document.getElementsById("clock").textContent = new Date().toLocaleTimeString();
+//   },500);
+// }
+// function stop(){
+//   clearInterval(timer);
+// }
+
+// document.getElementById("btn").addEventListener("click", ()=>{
+//   document.getElementById("msg").textContent = "Button was Clicked";
+// });
+
+// document.getElementById("name").addEventListener("keyup", (e)=>{
+//   document.getElementById("greet").textContent = "Hello "+ e.target.value;
+// });
+
+// document.getElementById("myForm").addEventListener("submit", function(e){
+//   e.preventDefault();
+//   const name = document.getElementById("username").value;
+//   document.getElementById("output").textContent = "Welcome, "+name;
+// });
+
+// function changeText(){
+//   document.getElementById("title").textContent = "Welcome to JavaScript";
+// }
+
+
+// function addItem(){
+//   let li = document.createElement("li");
+//   li.textContent = "banana";
+//   document.getElementById("list").appendChild(li);
+// }
+
+// function removePara(){
+//   let p = document.getElementById("removeMe");
+//   p.remove();
+// }
+// Simple Object
+let Person = {
+  name: "Harry",
+  age: 21,
+  city: "Banglore"
+};
+
+console.log(Person["name"]);
+console.log(Person.age);
+// Object with Function(Method)
+let student={
+  name: "Dupe",
+  marks: 90,
+  greet: function(){
+    return "Hello, "+ this.name;
+  }
+};
+console.log(student.greet());
+// Nested Objects
+let user = {
+  id: 1,
+  profile: {
+    username: "dupe@22",
+    email: "dupe22@gmail.com"
+  }
+};
+console.log(user.profile.email);
+// Array of Objects
+let products = [
+  {name: "Laptop",price:50000},
+  {name: "Phone", price: 20000},
+  {name: "Tablet", price: 15000}
+];
+products.forEach(p => console.log(p.name + " - " +p.price));
+// Everything in JS is an Object(Arrays, Functions, DOM elements)
+// Objects are used in APIs
+// They are the base for OOP(Object-Oriented Programming).
+let fruits = ["Apple", "Banana", "Mango"];
+console.log(fruits[0]);
+console.log(fruits[2]);
+
+let numbers = [1,2,3,4];
+numbers.forEach(n => console.log(n*2));
+
+let nums = [10,20,30,40];
+
+let doubled = nums.map(n => n*2);
+
+let above20 = nums.filter(n => n >20);
+
+let sum = nums.reduce((acc, n)=> acc+n, 0);
+// Used to store lists of data(users,products,scores, etc.)
+// Most real-world JS problems use arrays(like rendering products on amazon,comments on Youtube.etc.)
+// You'll combine arrays + object a lot in projects
+// Functions
+function greet(name){
+  return 'Hello '+name;
+}
+console.log(greet("harry"));
+const greet = function(name){
+  return "Hello "+name;
+};
+console.log(greet("Vidhya"));
+// Arrow
+const greet = (name) => "Hello, "+name;
+console.log(greet("World"));
+// Default Parameters
+function multiply(a, b=2){
+  return a*b;
+}
+console.log(multiply(5));
+console.log(multiply(5,3));
+// Rest Parameters
+function sum(...numbers){
+  return numbers.reduce((acc,n)=> acc+n, 0);
+}
+console.log(sum(1,2,3,4));
+// Functions returning Functions
+function outer(x){
+  return function inner(y){
+    return x + y;
+  };
+}
+const add5 = outer(5);
+console.log(add5(10));
+
+const book = {title: "JS Guide", author: "MDN"};
+const{title,author} = book
+console.log(title);
+console.log(author);
+// All code you write in JS is inside functions(event handlers, API calls,utilities, etc)
+// Functions power callbacks and higher-order-functions(like map,filter,reduce)
+// They connect with events(onclick, onchange), timers(setTimeout), and async code.
+// "this" keyword in JS
+
+// "this" in a simple object
+const person = {
+  name: "Vidhya",
+  greet: function(){
+    console.log("Hello I am ", +this.name);
+  }
+};
+person.greet();
+// "this" in normal function
+function showThis(){
+  console.log(this);
+}
+showThis();  //In browser => window, In strict mode -> undefined
+
+// "this" inside an object but using arrow function
+const userr = {
+  name: "Priya",
+  greet: () => {
+    console.log("Hello "+this.name);
+  }
+};
+userr.greet(); //Hello undefined because arrow functions do not bind their own "this"
+
+// "this" in a constructor function
+function Person(name){
+  this.name = name;
+}
+const p1 = new Person("Harry");
+console.log(p1.name);
+
+// With "new", "this" refers to the new object being created
+
+// Object Method => this =that object
+// Normal function => this =global(window) or undefined in strict Mode
+// Arrow Function => no own "this", takes from outer scope
+// Constructor => this = new object
+// Event handler => this = element
+// 
---
+++ prereq React/learn.js
@@ -0,0 +1,269 @@
+// ES6 Basics
+//  let and const
+// let => Block-scoped only works inside {} where it is assigned, re-assignable but cannot be redeclared in the same scope
+// const => Block-scoped, cannot be re-assigned (but objects/arrays inside can mutate it's content, not the reference)
+// var => re-declaration is allowed
+let name = "Vidhya";
+name= "robert";
+
+const age = 22;
+// age = 23;   // not allowed
+
+const arr = [1,2];
+arr.push(3);
+
+// Arrow Functions
+// shorter syntax, they don't bind their own "this".
+
+function add(a, b){
+  return a+b;
+}
+
+const addArrow = (a,b) => a+b;
+console.log(addArrow(5,3));
+
+
+let nums = [1,2,3,4];  // normal function
+let doubled1 = nums. map(function(n){
+  return n * 2
+});
+let doubled2 = nums.map(n=> n*2);
+console.log(doubled1);
+console.log(doubled2);
+
+// With no params
+const greet = () => "Hello World"
+console.log(greet());
+
+// Inside setTimeout
+setTimeout(() =>{
+  console.log("Executed after 1 second");
+}, 1000);
+
+
+// Spread Operator
+
+let arr1 = [1,2];
+let arr2 = [3,4];
+let combined = [...arr1, ...arr2];
+console.log(combined);
+
+// Rest operator
+// it gathers the remaining arguments into an array
+// Collect into one
+// Only one rest parameter per function
+// It must be the last parameter
+function fn(a,b, ...rest){
+
+}
+function sum(...nums){
+  return nums.reduce((acc,curr)=> acc+n, 0);
+}
+sum(1,2,3,4);
+sum();
+
+
+
+
+
+
+
+// Template Literals
+let user = "Vidhya";
+let greeting = `Hello, ${user}! Welcome to JavaScript`;
+console.log(greeting);
+
+let naame = "Riya";
+let agee = 22;
+let country = "India";
+let msg = `Welcone ${name}! You are ${age} years old and you live in ${country}`
+console.log(msg);
+
+
+
+let a =5, b=10;
+console.log(`The sum of ${a} and ${b} is ${a+b}`);
+// Function usage
+function welcome(user){
+  return `Welcome, ${user}!`;
+}
+console.log(welcome("Harry"));
+
+// Destructuring
+let colors = ["red", "green", "blue"]
+let [first, second] = colors;
+console.log(first, second);
+
+let [aa, , c] = [10,20,30];
+console.log(aa,c);  //O/p => 10 30
+
+let[x, y = 5] =[1];
+console.log(x, y);  // 1 5
+
+let person = {name: "Riya", age:22}    //Object
+let {nme, ag} = person;
+console.log(nme, ag);
+// Renaming
+let {name: username} = person;
+console.log(username);
+
+// Default Parameters
+function greet(name = "Guest"){
+  return `Hello, ${name}`;
+}
+console.log(greet());
+console.log(greet("Vidhya"));
+
+function multiply(a, b=2){
+  return a *b;
+}
+console.log(multiply(5));  //10
+console.log(multiply(5,3));  // 15
+
+// With arrow function
+const power = (base, exp = 2) => base ** exp;
+console.log(power(3));
+console.log(power(3,3));
+
+
+// Object Literals 
+// An object literal is the simplest way of creating objects using {}
+
+let Uname = "Vidhya";
+let Age = 20;
+let User = {Uname, Age};
+console.log(user);
+
+
+// "THIS" KEYWORD
+// Global
+console.log(this); // window(in browser)
+
+// Iside Object
+let oUser = {
+  name: "Vidhya",
+  show: function(){
+    console.log(this.name);
+  }
+};
+oUser.show();
+
+// Arrow Funtion
+// Arrow fucntions are shorter way to write functions introduces in ES6
+let Ouser = {
+  name: "Vidhya",
+  show: () =>{
+    console.log(this.name);
+  }
+};
+user.show();  //undefined  (arrow functin doesn;t bind it's own "this")
+
+// Constructor function
+function person(name){
+  this.name = name;
+}
+let p = new person("Priya");
+console.log(p.name);
+
+
+function Person(){
+  this.age = 20;
+  setTimeout(()=>{
+    this.age++;
+  }, 1000);
+}
+new Person();
+
+
+// Array Methods
+// 1) FILTER() -> returns elements matching condition
+let num = [1, 2, 3, 4, 5];
+let evens = nums.filter(n => n % 2 === 0);
+console.log(evens);
+// Gets adults age
+let people = [
+  {name: "Riya", age: 21},
+  {name: "rahul", age: 19},
+  {name: "Anu", age: 19}
+];
+let adults = people.filter(p => p.age >=18);
+console.log(adults);
+
+// words longer than 5 letters
+let words = ["cat", "elephant","dog","giraffe"];
+let longWords = words.filter(w => w.length > 5);
+console.log(longWords);
+
+// 2) MAP() -> Transforms elements
+let numb = [1,2,3];
+let doubled = numb.map(n => n * 2);
+console.log(doubled);
+
+let fruits = ["apple", "banana", "mango"];
+let upper = fruits.map(fruit => fruit.toUpperCase());
+console.log(upper);
+// Extract property from Objects
+let users = [
+  {name: "Riya", age:22},
+  {name: "rahul", age: 25},
+  {name: "Anu", age: 19}
+];
+let names = users.map(user => user.name);
+console.log(names);
+
+
+// 3) REDUCE() => reduces to a single value
+let nom = [1,2,3,4];
+let sum = nom.reduce((acc,curr) => acc+curr,0);
+console.log(sum); 
+
+let numbers = [10, 25, 8, 99, 45];
+let max = numbers.reduce((acc, curr)=> acc > curr ? acc: curr);
+console.log(max);
+
+let vegs = ["cucumber", "carrot", "beetroot","cabbage","beans"];
+let counts = vegs.reduce((acc,veg)=>{
+  acc[veg] = (acc[veg] || 0) + 1;
+  return acc;
+}, {});
+
+
+// Counter
+let count = 0;
+const step = 1;
+
+function increase(){
+  count += step;
+  document.getElementById("count"),textContent = count;
+}
+function decrease(){
+  count -= step;
+  document.getElementById("count").textContent =count;
+}
+
+// Chaining map(), filter(),reduce()
+// Double even numbers
+let numbbers = [1,2,3,4,5,6];
+let result = numbbers.filter(n => n % 2 === 0).map(n => n *2);
+console.log(result);
+
+// Total Age of Adults 
+let peoples = [
+  {name: "Priya", age: 22},
+  {name: "Rahul", age: 21},
+  {name: "Dona", age: 28}
+];
+let totalAge = peoples.filter(p => p.age >=18).map(p =>p.age).reduce((sum,age)=> sum +age, 0);
+console.log(totalAge);
+
+let swords = ["cat", "elephant","dog","giraffe"];
+let longest = words.reduce((longest, sword)=>
+words.length > longest.length ? word : longest);
+console.log(longest);
+
+let key = "score"
+let player = {
+  name: "Ria",
+  [key]: 100
+}
+console.log(player);
