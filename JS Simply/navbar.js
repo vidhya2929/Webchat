05090e57c3861afa98093525457528697375171e
Vidhya <vidhyamrobert.29@gmail.com>
2025-08-24 21:18:56 +0530

    jswrap4

--- JS Simply/navbar.js
+++ JS Simply/navbar.js
@@ -27,3 +27,18 @@ searchInput.addEventListener("keyup", ()=>{
     }
   });
 });
+
+function checkPalindrom(palindrom)
+{
+
+    for( var i = palindrom.length; i > 0; i-- )
+    {
+        if( palindrom[i] = palindrom.charAt(palindrom.length)-1 )
+        {
+            document.write('the word is palindrome.');
+        }else{
+            document.write('the word is not palindrome!');
+        }
+    }
+}
+checkPalindrom('wordthatwillbechecked');
